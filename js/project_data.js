6340638ce81cd7e0c31209b1b5ed27f31a495562
Senko <85813668+thatonesenko@users.noreply.github.com>
2022-10-15 06:10:56 +1300

    Main
    Added: Others Catgory

--- js/project_data.js
+++ js/project_data.js
@@ -1,9 +1,11 @@
 const ProjectData = {
+	// NODEJS
 	"azuki": {
 		title: "Azuki",
 		github: "https://discord.gg/xC7yrgwQMr",
 		description: "Azuki is a <a href=\"https://discordapp.com/\">Discord</a> bot that I built to be used in growing content-creator guilds. This was the first project that I made that I'm still maintaining to this day."
 	},
+	// HTML,CSS
 	"s_control": {
 		title: "Senko Control Panel",
 		github: "https://twitch.tv/roryau_",
@@ -13,5 +15,11 @@ const ProjectData = {
 		title: "Portfolio",
 		github: "https://github.com/thatonesenko/thatonesenko.github.io",
 		description: "You're looking at it! This website was a lot of work and I'm really proud of some of the results I got, such as the svg animations and the overall aesthetic."
+	},
+	// OTHERS
+	"pt_game": {
+		title: "??? Game",
+		github: "https://discord.gg/xC7yrgwQMr",
+		description: "A game that I'm currently developing with some friends, that will most likely release on steam someday in the future"
 	},
 };
